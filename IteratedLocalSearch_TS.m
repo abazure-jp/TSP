e1c15bf10dc5a633561f5f64983d965fcb8441b4
abazure_jp <say.0213@gmail.com>
2016-11-17 19:33:12 +0900

    Test commit
    This commit be made to test

--- IteratedLocalSearch_TS.m
+++ IteratedLocalSearch_TS.m
@@ -8,12 +8,12 @@ clear all;
 
 %% --- Create cities and map
 nStops = 100; % you can use any number, but the problem size scales as N^2
-[distMap stopsLon stopsLat] = initCities(nStops);
+[distMap, stopsLon, stopsLat] = initCities(nStops);
 
 %% --- params of TabuSearch
-times = 99; % 探索の回数
+times = 499; % 探索の回数
 timesNeighbor = 30; % 近傍探索の回数
-sizeTabuList = timesNeighbor * times * 0.3;
+sizeTabuList = times * 0.4;
 
 %% --- params of IteratedLocalSearch
 iterate = 19;
@@ -25,7 +25,7 @@ initTour = getRandomTour(nStops);
 %% --- Step2: LocalSearch(tour)
 % This code use Tabu Search (2-opt) as local search.
 doPlot = 0;
-[bestCost bestTour] = doTabuSearch(distMap,stopsLon,stopsLat,times,timesNeighbor,sizeTabuList,nStops,initTour,doPlot);
+[bestCost, bestTour] = doTabuSearch(distMap,stopsLon,stopsLat,times,timesNeighbor,sizeTabuList,nStops,initTour,doPlot);
 bestCosts(1,1) = bestCost;
 
 %% --- Step3: LocalSearch(tour)
@@ -38,7 +38,7 @@ bestCosts(1,1) = bestCost;
 
 for i = 1:iterate
   nextInitTour = getInversedTour(bestTour);
-  [bestCost bestTour] = doTabuSearch(distMap,stopsLon,stopsLat,times,timesNeighbor,sizeTabuList,nStops,initTour,doPlot);
+  [bestCost, bestTour] = doTabuSearch(distMap,stopsLon,stopsLat,times,timesNeighbor,sizeTabuList,nStops,initTour,doPlot);
   bestCosts(i+1,1) = bestCost;
 end
 
