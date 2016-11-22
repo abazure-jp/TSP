2744a508523a013383e3dc06fdc5d9c21ba0147e
abazure_jp <say.0213@gmail.com>
2016-11-22 17:25:23 +0900

    Fix method about how to change in getNOpt

--- assets/getNOpt.m
+++ assets/getNOpt.m
@@ -1,10 +1,17 @@
 function tour = getNOpt(tour,N)
   nStops = size(tour,2);
-  cities = getNRandomCities(N,nStops); % 1以外かつ重複しないランダムな都市番号がN個入っている
-  temp = sort(cities); % これをソート
+  indx = getNRandomCities(N,nStops); % 1以外かつ重複しないランダムな番号がN個入っている
 
+  temp = zeros(size(N,2));
+
+  % indx番目に訪れる都市の番号をtempへ保存
   for i = 1:N
-    tour(temp(i)) = cities(i)
+    temp(i) = tour(indx(i));
   end
 
+  % temp(i)をsortしてtourに入れ直す。
+  temp = sort(temp);
+  for i = 1:N
+    tour(indx(i)) = temp(i);
+  end
 end
