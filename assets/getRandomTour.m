88028cfd06d4417433d410a049923b7c4a1e8969
abazure_jp <say.0213@gmail.com>
2016-11-26 15:13:56 +0900

    Remove a wasteful row to refactoring

--- assets/getRandomTour.m
+++ assets/getRandomTour.m
@@ -5,17 +5,14 @@ function tour = getRandomTour(numOfCities)
 
   % passedCities: 行番号は都市番号。各要素にはブール代数が入り、該当都市が通過済であれば1、でなければ0
   passedCities = zeros(1,numOfCities);
-  % 開始地点はすでに通過しているものとする
-  passedCities(1,1) = 1;
-
-  % passedCitiesを元に、次の行き先の都市番号が格納されたベクトルを作成する。
-  nextList = getNextList(passedCities);
+  passedCities(1,1) = 1; % 開始地点はすでに通過しているものとする
 
   for i = 1:numOfCities-1
+    % passedCitiesを元に、次の行き先の都市番号が格納されたベクトルを作成する。
+    nextList = getNextList(passedCities);
     % 次の候補地を決める。
     nextIs = nextList(1,randi(size(nextList,2)));
     tour(1,i+1) = nextIs;
     passedCities(1,nextIs) = 1;
-    nextList = getNextList(passedCities);
   end
 end
