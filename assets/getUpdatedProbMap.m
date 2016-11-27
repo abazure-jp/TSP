c96ef003bb26e5213eddfc0daa009a50a887edf0
abazure_jp <say.0213@gmail.com>
2016-11-28 03:40:35 +0900

    Make some file to ACO
    めっちょつかれた…

---
+++ assets/getUpdatedProbMap.m
@@ -0,0 +1,14 @@
+function probMap = getUpdatedProbMap(acidMap,heuristicsMap,acidPow,heurisPow)
+  tourSize = size(acidMap,1);
+  % evaporation
+  for i = 1:tourSize
+    for j = i + 1:tourSize
+      if acidMap(i,j) == 0
+        probMap(i,j) = 1*heuristicsMap(i,j)^heurisPow;
+      else
+        probMap(i,j) = acidMap(i,j)^acidPow*heuristicsMap(i,j)^heurisPow;
+      end
+    end
+  end
+  probMap(tourSize,:) = NaN;
+end
